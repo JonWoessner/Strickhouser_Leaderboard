3c8dcbe8d364bc125ce5253d40498c60c536e61e
Emil D <265544937+Emil-D-0@users.noreply.github.com>
2026-04-17 08:29:25 -0400

    Added main template for the leaderboards and modified some other stuff along with a style sheet

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    page_title = "Title of thou Home"
+    return render_template('index.html', title=page_title)
+
+
+@app.route('/leaderboard_base')
+def lead_base():
+    page_title = "Game"
+    scores = [
+    {'player': 'Ada', 'score': 9800, 'year': '20XX'},
+    {'player': 'Grace', 'score': 8750, 'year': '20XX'},
+    {'player': 'Alan', 'score': 7200, 'year': '20XX'},
+    {'player': 'Linus', 'score': 6500, 'year': '20XX'},
+    {'player': 'Bjarne', 'score': 5900, 'year': '20XX'},
+    {'player': 'Kevin', 'score': 4000, 'year': '20XX'},
+    {'player': 'Luke', 'score': 10450, 'year': '20XX'},
+    {'player': 'Ava', 'score': 7780, 'year': '20XX'}
+    ]
+    noscores = []
+    return render_template(
+    'leaderboard_base.html', 
+    score_count = len(scores),
+    title = page_title,
+    scores = scores
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
